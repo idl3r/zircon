25d5f19844230abd0f258d6aedbfa9029e995e2a
Yvonne Yip <yky@google.com>
2018-01-10 14:45:04 -0800

    [dev][sdhci][sdmmc] driver clean up
    * remove internal usage of iotxn
    * introduce SDMMC protocol instead of device_ioctl()
    * fix PIO mode
    * make tuning command less special
    * fix various issues where we did not conform to the spec
    * comment out bit rotted SD card support for now
    
    Change-Id: I7b8e96d326cfe010d1a8e1355453ac89790d5498

--- system/dev/block/sdmmc/sdmmc.h
+++ system/dev/block/sdmmc/sdmmc.h
@@ -7,29 +7,30 @@
 #include <zircon/compiler.h>
 
 #include <ddk/device.h>
+#include <ddk/protocol/block.h>
+#include <ddk/protocol/sdmmc.h>
 #include <hw/sdmmc.h>
 
 #include <threads.h>
 
 __BEGIN_CDECLS;
 
-typedef struct sdmmc {
+typedef enum sdmmc_type {
+    SDMMC_TYPE_SD,
+    SDMMC_TYPE_MMC,
+} sdmmc_type_t;
+
+typedef struct sdmmc_device {
     zx_device_t* zxdev;
-    zx_device_t* host_zxdev;
 
-    uint8_t type;
-#define SDMMC_TYPE_SD   0
-#define SDMMC_TYPE_MMC  1
+    sdmmc_protocol_t host;
+    sdmmc_host_info_t host_info;
 
-    uint8_t bus_width;      // Data bus width
-    uint8_t signal_voltage; // Bus signal voltage
+    sdmmc_type_t type;
 
-    uint8_t timing;         // Bus timing
-#define SDMMC_TIMING_LEGACY 0
-#define SDMMC_TIMING_HS     1
-#define SDMMC_TIMING_HSDDR  2
-#define SDMMC_TIMING_HS200  3
-#define SDMMC_TIMING_HS400  4
+    sdmmc_bus_width_t bus_width;
+    sdmmc_voltage_t signal_voltage;
+    sdmmc_timing_t timing;
 
     unsigned clock_rate;    // Bus clock rate
     uint64_t capacity;      // Card capacity
@@ -48,13 +49,13 @@ typedef struct sdmmc {
     zx_handle_t worker_event;
     bool worker_thread_running;
 
-    uint32_t max_transfer_size;
-} sdmmc_t;
+    block_info_t block_info;
+} sdmmc_device_t;
 
-// Issue a command to the host controller
-zx_status_t sdmmc_do_command(zx_device_t* dev, const uint32_t cmd, const uint32_t arg, iotxn_t* txn);
+// Issue a request to the host controller and wait for completion.
+zx_status_t sdmmc_do_request(sdmmc_device_t* dev, sdmmc_request_t* req);
 
-zx_status_t sdmmc_probe_sd(sdmmc_t* sdmmc, iotxn_t* setup_txn);
-zx_status_t sdmmc_probe_mmc(sdmmc_t* sdmmc, iotxn_t* setup_txn);
+zx_status_t sdmmc_probe_sd(sdmmc_device_t* dev);
+zx_status_t sdmmc_probe_mmc(sdmmc_device_t* dev);
 
 __END_CDECLS;
